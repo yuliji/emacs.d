75adcb19346250ba06fa102ee129fe9c14d4674a
yuliji <liji.yu@seedlinktech.com>
2016-08-03 14:10:43 +0800

    Add company-jedi

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,4 +2,10 @@
 (global-hl-line-mode t)
 
 (setq-default c-basic-offset 4)
+
+(defun my/python-mode-hook ()
+  (add-to-list 'company-backends 'company-jedi))
+
+(add-hook 'python-mode-hook 'my/python-mode-hook)
+
 (provide 'init-local)
